6f10a4b269eed31ee2bf9ef17b3528428e15fc78
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-02-02 16:09:35 +0700

    Update writer.py

--- plugins/writer.py
+++ plugins/writer.py
@@ -71,12 +71,12 @@ async def writer(e):
     k = await e.eor(get_string("com_1"))
     img = Image.open("resources/extras/template.jpg")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("resources/fonts/assfont.ttf", 30)
+    font = ImageFont.truetype("resources/fonts/DroidSansMono.ttf", size=36)
     x, y = 150, 140
     lines = text_set(text)
-    line_height = font.getsize("hg")[1]
+    line_height = font.getbbox("\n")[3]
     for line in lines:
-        draw.text((x, y), line, fill=(1, 22, 55), font=font)
+        draw.text((x, y), line, fill=(5, 22, 55), font=font)
         y = y + line_height - 5
     file = "ult.jpg"
     img.save(file)
