b90e7d867480999fa465eb1aafcaca5dc99b27f5
TrueSaiyan <ufperth@protonmail.com>
2024-02-13 02:20:49 +0800

    Update bot.py

--- plugins/bot.py
+++ plugins/bot.py
@@ -184,10 +184,10 @@ async def lol(ult):
 
 async def mention_user(user_id):
     try:
-        user_entity = await ultroid_bot.get_entity(user_id)
+        user_entity = await event.client.get_entity(user_id)
         first_name = user_entity.first_name
-        mention_text =  [{user.first_name}](tg://user?id={user.id})
-        return mention_text
+        mention_text = f"[{first_name}](tg://user?id={user_id})"
+        await event.reply(mention_text)
     except Exception as e:
         print(f"Failed to mention user: {e}")
 
