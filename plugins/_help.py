ff228053b4caee93157967fcff784c708550ff22
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-02-14 22:16:24 +0700

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -24,7 +24,7 @@ _main_help_menu = [
     ],
     [
         Button.inline(get_string("help_6"), data="uh_VCBot_"),
-        #Button.inline(get_string("help_7"), data="inlone"),
+        Button.inline(get_string("help_7"), data="inlone"),
     ],
     [
         Button.inline(get_string("help_8"), data="ownr"),
@@ -32,7 +32,7 @@ _main_help_menu = [
             get_string("help_9"), url=f"https://t.me/{asst.me.username}?start=set"
         ),
     ],
-    #[Button.inline(get_string("help_10"), data="close")],
+    [Button.inline(get_string("help_10"), data="close")],
 ]
 
 
