26326d45939b28304318b50bd092c07ca29d2923
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-02-20 20:09:33 +0700

    Update _inline.py

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -60,9 +60,9 @@ SUP_BUTTONS = [
 
 @in_pattern(owner=True, func=lambda x: not x.text)
 async def inline_alive(o):
-    TLINK = inline_pic() or "https://graph.org/file/74d6259983e0642923fdb.jpg"
+    #TLINK = inline_pic() or "https://graph.org/file/74d6259983e0642923fdb.jpg"
     MSG = "• **Ultroid Userbot •**"
-    WEB0 = InputWebDocument(
+    #WEB0 = InputWebDocument(
         "https://graph.org/file/acd4f5d61369f74c5e7a7.jpg", 0, "image/jpg", []
     )
     RES = [
