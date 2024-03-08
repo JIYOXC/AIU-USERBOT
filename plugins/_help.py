d7a11d66221038b0cf1678cc7e3eb59d7bfcb312
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-03-08 14:26:59 +0700

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -20,7 +20,7 @@ from . import HNDLR, LOGS, OWNER_NAME, asst, get_string, inline_pic, udB, ultroi
 _main_help_menu = [
     [
         Button.inline(get_string("help_4"), data="uh_Official_"),
-        Button.inline(get_string("help_5"), data="uh_Addons_"),
+        #Button.inline(get_string("help_5"), data="uh_Addons_"),
     ],
     [
         #Button.inline(get_string("help_6"), data="uh_VCBot_"),
@@ -28,11 +28,11 @@ _main_help_menu = [
     ],
     [
         Button.url(get_string("help_8"), url=f"https://t.me/aiu_ubot?start"),
-        Button.inline(get_string("help_8"), data="ownr"),
-        Button.url(get_string("help_9"), url=f"https://t.me/{asst.me.username}?start=set"
+        #Button.inline(get_string("help_8"), data="ownr"),
+        #Button.url(get_string("help_9"), url=f"https://t.me/{asst.me.username}?start=set"
         ),
     ],
-    #[Button.inline(get_string("help_10"), data="close")],
+    [Button.inline(get_string("help_10"), data="close")],
 ]
 
 
