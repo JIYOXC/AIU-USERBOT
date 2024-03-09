668f73558ec0c20fcae9a3c4a9cc5b723641700d
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-03-09 20:15:17 +0700

    Update _inline.py

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -51,7 +51,7 @@ upage = 0
 SUP_BUTTONS = [
     [
         Button.url("• Repo •", url="https://github.com/JIYOXC/AIU-USERBOT"),
-        Button.url("• Support •", url="t.me/UltroidSupportChat"),
+        Button.url("• Support •", url="t.me/aiusupportt"),
     ],
 ]
 
