36306817bb6dff16bf34c3ec9b33de4f29f62ba7
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-03-12 14:23:39 +0700

    Add files via upload

---
+++ plugins/asupan.py
@@ -0,0 +1,27 @@
+# 🍀 © @tofik_dn
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+# ⚠️ Do not remove credits
+
+from secrets import choice
+
+from telethon.tl.types import InputMessagesFilterVideo, InputMessagesFilterVoice
+
+
+@ultroid_cmd(pattern="asupan$")
+async def _(event):
+    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@tedeasupancache", filter=InputMessagesFilterVideo
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan video asupan.**")
+
