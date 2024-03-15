9df83615bbcea6b0d7b75ebadb43389707be2afb
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-03-15 23:24:14 +0700

    Update asupan.py

--- plugins/asupan.py
+++ plugins/asupan.py
@@ -27,3 +27,38 @@ async def _(event):
         await xx.delete()
     except Exception:
         await xx.edit("**Tidak bisa menemukan video asupan.**")
+
+@ultroid_cmd(pattern="pap$")
+async def _(event):
+    xx = await event.eor("🔍 Please wait...")
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@mm_kyran", filter=InputMessagesFilterVideo
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa pap.**")
+
+@ultroid_cmd(pattern="ppcp$")
+async def _(event):
+    xx = await event.eor("🔍 Please wait...")
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@ppcpcilik", filter=InputMessagesFilterVideo
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan pap couple.**")
+
