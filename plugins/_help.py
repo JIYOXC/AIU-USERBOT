d60bbbea5d7e1604e135636ee0bc306df133dcc1
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-03-15 23:34:12 +0700

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -44,19 +44,19 @@ async def _help(ult):
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Official"][plug]:
                     output += i
-                output += "\n© @aiusupportt"
+                output += "\n© @AIUSUPPORTT"
                 await ult.eor(output)
             elif HELP.get("Addons") and plug in HELP["Addons"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Addons"][plug]:
                     output += i
-                output += "\n© @aiusupportt"
+                output += "\n© @AIUSUPPORTT"
                 await ult.eor(output)
             elif HELP.get("VCBot") and plug in HELP["VCBot"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["VCBot"][plug]:
                     output += i
-                output += "\n© @aiusupportt"
+                output += "\n© @AIUSUPPORTT"
                 await ult.eor(output)
             else:
                 try:
@@ -64,7 +64,7 @@ async def _help(ult):
                     for d in LIST[plug]:
                         x += HNDLR + d
                         x += "\n"
-                    x += "\n© @aiusupportt"
+                    x += "\n© @AIUSUPPORTT"
                     await ult.eor(x)
                 except BaseException:
                     file = None
@@ -99,7 +99,7 @@ async def _help(ult):
                     elif HELP.get("VCBot") and file in HELP["VCBot"]:
                         for i in HELP["VCBot"][file]:
                             output += i
-                    output += "\n© @aiusupportt"
+                    output += "\n© @AIUSUPPORTT"
                     await ult.eor(output)
         except BaseException as er:
             LOGS.exception(er)
