873760cd1e4fdf61ac4cd5ac8adc384ddc71d65a
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-04-16 14:39:06 +0700

    Update gcast.py

--- plugins/gcast.py
+++ plugins/gcast.py
@@ -15,7 +15,7 @@ import os
 
 import heroku3
 from telethon.errors import FloodWaitError
-
+from pyUltroid.configs import BLACKLIST_GCAST
 #from pyUltroid.dB.blacklist_db import BLACKLIST_DB
 from pyUltroid.dB.base import KeyManager
 from . import get_help, HEROKU_API_KEY, HEROKU_APP_NAME
