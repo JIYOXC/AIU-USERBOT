ae435a196780732caf9c2e1421e96c1545ba5552
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-10-07 22:19:38 +0700

    Update vctools.py

--- plugins/vctools.py
+++ plugins/vctools.py
@@ -19,6 +19,20 @@
 • `{i}vcinvite`
     Invite all members of group in Group Call.
     (You must be joined)
+• `{i}mutevc`
+   Mute playback.
+
+• `{i}unmutevc`
+   UnMute playback.
+
+• `{i}pausevc`
+   Pause playback.
+
+• `{i}resumevc`
+   Resume playback.
+
+• `{i}replay`
+   Re-play the current song from the beginning.
 """
 
 from telethon.tl.functions.channels import GetFullChannelRequest as getchat
@@ -27,8 +41,7 @@ from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
 from telethon.tl.functions.phone import EditGroupCallTitleRequest as settitle
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
-
-from . import get_string, ultroid_cmd
+from . import get_string, ultroid_cmd, vc_asst, Player
 
 
 async def get_call(event):
@@ -103,3 +116,78 @@ async def _(e):
         await e.eor(get_string("vct_2").format(title))
     except Exception as ex:
         await e.eor(f"`{ex}`")
+
+@vc_asst("mutevc")
+async def mute(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        try:
+            chat = await event.client.parse_id(chat)
+        except Exception as e:
+            return await event.eor(f"**ERROR:**\n{str(e)}")
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat)
+    await ultSongs.group_call.set_is_mute(True)
+    await event.eor(get_string("vcbot_12"))
+
+
+@vc_asst("unmutevc")
+async def unmute(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        try:
+            chat = await event.client.parse_id(chat)
+        except Exception as e:
+            return await event.eor(f"**ERROR:**\n{str(e)}")
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat)
+    await ultSongs.group_call.set_is_mute(False)
+    await event.eor("`UnMuted playback in this chat.`")
+
+
+@vc_asst("pausevc")
+async def pauser(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        try:
+            chat = await event.client.parse_id(chat)
+        except Exception as e:
+            return await event.eor(f"**ERROR:**\n{str(e)}")
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat)
+    await ultSongs.group_call.set_pause(True)
+    await event.eor(get_string("vcbot_14"))
+
+
+@vc_asst("resumevc")
+async def resumer(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        try:
+            chat = await event.client.parse_id(chat)
+        except Exception as e:
+            return await event.eor(f"**ERROR:**\n{str(e)}")
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat)
+    await ultSongs.group_call.set_pause(False)
+    await event.eor(get_string("vcbot_13"))
+
+
+@vc_asst("replay")
+async def replayer(event):
+    if len(event.text.split()) > 1:
+        chat = event.text.split()[1]
+        try:
+            chat = await event.client.parse_id(chat)
+        except Exception as e:
+            return await event.eor(f"**ERROR:**\n{str(e)}")
+    else:
+        chat = event.chat_id
+    ultSongs = Player(chat)
+    ultSongs.group_call.restart_playout()
+    await event.eor("`Re-playing the current song.`")
+    
